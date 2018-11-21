bdfa7eb75ebaa09a88e04c929f85c357e4f95027
Diego D. Costa <diegocostadeveloper@gmail.com>
2018-11-20 22:49:18 -0200

    - removido debugs - inputs html - pendente refinamento da aplicação (css) e limpeza dos campos

--- projeto/src/app/shared/cadastrar-notas/cadastrar-notas.component.ts
+++ projeto/src/app/shared/cadastrar-notas/cadastrar-notas.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-import { CadastrarNotas } from "./cadastrar-notas";
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -30,30 +28,7 @@ export class CadastrarNotasComponent implements OnInit {
       valor:['',Validators.required]
     });
 
-    //debug
-    /*
-    console.log(this.notas_cadastradas);
-    this.notas_cadastradas.push({
-      loja:"Renner",
-      numero: 3123,
-      data: "01022018",
-      pagamento:"boleto",
-      valor: 58700
-    })
-    */
   }
-
-  /*public cadastrarNotas(){
-    this.notas_cadastradas.push(
-      {
-        loja: this.loja,
-        numero: 3123,
-        data: "01022018",
-        pagamento:"boleto",
-        valor: 58700
-      }
-    )
-  }*/
 
   // convenience getter for easy access to form fields
   get f() { return this.formCadastraNota.controls; }
@@ -69,12 +44,10 @@ export class CadastrarNotasComponent implements OnInit {
     this.notas_cadastradas.push(this.formCadastraNota.value);
 
     //debug
-    console.log(this.formCadastraNota.value);
-    alert('SUCCESSO!! :-)\n\n' + JSON.stringify(this.formCadastraNota.value))
+    //console.log(this.formCadastraNota.value);
+    //alert('SUCCESSO!! :-)\n\n' + JSON.stringify(this.formCadastraNota.value))
 
     //limpa o form após o cadastro
     this.formCadastraNota.reset();
-
-}
-
+  }
 }
